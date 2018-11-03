fd9a91e5ca98a3e68ed5bfa7a73dd44598a1c09d
Sarah Schaller <sarahcschaller@gmail.com>
2018-11-03 15:14:17 -0400

    import components and browserrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+
 import './App.css';
 
 class App extends Component {
